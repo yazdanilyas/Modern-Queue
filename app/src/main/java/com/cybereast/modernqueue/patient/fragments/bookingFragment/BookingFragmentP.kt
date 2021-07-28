4a506d6b6e42217da32af1b99f33e41cbd8000b4
Yazdan Ilyas <yazdanilyas@gmail.com>
2021-07-28 18:40:47 +0500

    Working on Booking Module on patient side

--- app/src/main/java/com/cybereast/modernqueue/patient/fragments/bookingFragment/BookingFragmentP.kt
+++ app/src/main/java/com/cybereast/modernqueue/patient/fragments/bookingFragment/BookingFragmentP.kt
@@ -1,11 +1,27 @@
 package com.cybereast.modernqueue.patient.fragments.bookingFragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cybereast.modernqueue.R
+import com.cybereast.modernqueue.adapters.BookingAdapter
+import com.cybereast.modernqueue.constants.Constants
 import com.cybereast.modernqueue.databinding.FragmentBookingPBinding
+import com.cybereast.modernqueue.doctor.ui.fragments.myBookings.MyBookingViewModel
+import com.cybereast.modernqueue.enums.BookingStatus
+import com.cybereast.modernqueue.listeners.RecyclerItemClickListener
+import com.cybereast.modernqueue.models.Booking
+import com.cybereast.modernqueue.models.Session
+import com.cybereast.modernqueue.utils.AppUtils
+import com.cybereast.modernqueue.utils.CommonKeys
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class BookingFragmentP : Fragment() {
@@ -18,6 +34,28 @@ class BookingFragmentP : Fragment() {
     }
 
     private lateinit var mBinding: FragmentBookingPBinding
+    private lateinit var mViewModel: BookingPViewModel
+    private var fireStoreDbRef = FirebaseFirestore.getInstance()
+    private val mAuth = FirebaseAuth.getInstance()
+    private lateinit var mAdapter: BookingAdapter
+    private var mRecyclerListener = object : RecyclerItemClickListener {
+        override fun onClick(data: Any?, position: Int) {
+            val session = data as Session
+            val bundle = Bundle()
+            Log.d("TAG", "onClick: ${session.sessionId}")
+        }
+
+        override fun onItemChildClick(view: View, data: Any?) {
+            val booking = data as Booking
+            setPatientBookingStatus(view, booking)
+        }
+
+        override fun onSeeProfile(data: Any?, position: Int) {
+
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,4 +68,71 @@ class BookingFragmentP : Fragment() {
         return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewModel = ViewModelProvider(this).get(BookingPViewModel::class.java)
+        getMyBookings()
+        setListeners()
+    }
+
+    private fun setListeners() {
+
+    }
+
+    private fun setUpRecycler() {
+        mAdapter =
+            BookingAdapter(mViewModel.bookingList, mRecyclerListener)
+        mBinding.bookingRecycler.layoutManager =
+            LinearLayoutManager(activity)
+        mBinding.bookingRecycler.setHasFixedSize(true)
+        mBinding.bookingRecycler.adapter = mAdapter
+    }
+
+    private fun getMyBookings() {
+        val query =
+            fireStoreDbRef.collection(Constants.COLLECTION_PATIENT_BOOKING)
+                .whereEqualTo("patientId", mAuth.currentUser?.uid).get()
+                .addOnSuccessListener { snapshot ->
+
+                    Log.d("TAG", "getMyBookings: " + snapshot.size())
+                    for (snap in snapshot) {
+                        val booking = snap.toObject(Booking::class.java)
+                        mViewModel.bookingList.add(booking)
+                        Log.d("TAG", "getMyBookings: $booking")
+                    }
+                    setUpRecycler()
+                }
+    }
+
+    private fun setPatientBookingStatus(view: View, booking: Booking) {
+        val popUp = PopupMenu(requireActivity(), view)
+        popUp.menuInflater.inflate(R.menu.menu_booking_options, popUp.menu)
+        popUp.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menu_consulting -> {
+                    setBookingStatus(view, booking, BookingStatus.CONSULTING.toString())
+                }
+                R.id.menu_consulted -> {
+                    setBookingStatus(view, booking, BookingStatus.CONSULTED.toString())
+                }
+            }
+            true
+        }
+        popUp.show()
+    }
+
+    private fun setBookingStatus(view: View, booking: Booking, status: String) {
+//        AppUtils.showHideProgressBar(mBinding.progressBar, View.VISIBLE)
+        val userMap = hashMapOf<String, Any>(
+            "bookingStatus" to status
+        )
+        val docRef = fireStoreDbRef.collection(Constants.COLLECTION_PATIENT_BOOKING)
+            .document(booking.bookingId.toString())
+        docRef.update(userMap).addOnSuccessListener {
+            AppUtils.showToast(requireContext(), "Status Updated")
+
+        }.addOnFailureListener {
+            AppUtils.showToast(requireContext(), it.message.toString())
+        }
+    }
 }
